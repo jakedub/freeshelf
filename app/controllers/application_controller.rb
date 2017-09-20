6043443812972ff3f3e5ec08fd142bcd6ac96cd1
jakedub <jacob.w.moore@gmail.com>
2017-09-20 09:21:00 -0400

    updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+#   before_action do
+#     @current_user = User.find(session[:id])
+#   end
+#
+#   def authenticate_user!
+#     if user_signed_in? != true
+#       redirect_to sign_in_path
+#     end
+#   end
+#
+#   def user_signed_in?
+#     @current_user.present?
+#   end
+# end
 end
