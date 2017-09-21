fe406dad33a81e410f5463a51af832f2cad37d44
jakedub <jacob.w.moore@gmail.com>
2017-09-21 10:13:48 -0400

    added migration for associations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def current_user
+   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+ end
+
 #   before_action do
 #     @current_user = User.find(session[:id])
 #   end
